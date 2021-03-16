d511ad9056a2b1770a9f654eb6726e10a2f0a1e2
Brendon Albertson <albertsonbrendon@gmail.com>
2021-03-16 13:33:17 -0400

    Add files via upload

--- sentences_fetcher.js
+++ sentences_fetcher.js
@@ -1,8 +1,6 @@
 
-var examplesArray = [];
 var wb = document.getElementById('wordbank');
 var searchTerms;
-var sentenceNumber = 1;
 
 var selectedSenElement;
 var selectedSenName;
@@ -15,26 +13,44 @@ var counterForNames = 0;
 var counterForIds = 0;
 var noOfSentences;
 
+var clozeSentences = [];
+
 var score = 0;
+
+var count;
+
+
 
 
 function main() {
+
+	if (document.getElementById('sentences-per-word').value == "") {
+		document.getElementById('alert').style.backgroundColor = 'yellow';
+		document.getElementById('alert').innerHTML += '<br><b>Please enter a number for sentences per word/phrase!</b>';
+		return;
+	}
+
+	document.getElementById('alert').style.backgroundColor = 'white';
+	document.getElementById('alert').innerHTML = 'Sentences per word/phrase:';
 
 	//clear everything from the previous search first:
 	document.getElementById('sentences').innerHTML = '';
 	document.getElementById('examples').innerHTML = '';
 	document.getElementById('wordbank').innerHTML = '';
 	document.getElementById('message').innerHTML = '';
-	examplesArray = [];
+	document.getElementById('start-button').innerHTML = 'Make Quiz';
+
 	counterForNames = 0;
+	counterForIds = 0;
+	score = 0;
 	clickedWord = false;
 	clickedSentence = false;
-	sentenceNumber = 1;
+	clozeSentences = [];
 
 	wb.style.visibility = 'visible';
 
 	//add the name line and directions
-	document.getElementById('header').innerHTML = '<br><br><p>Name: ________________________________________</p><p><strong>Directions: Write the correct word in each blank.  Use words from the box.</strong></p>';
+	document.getElementById('header').innerHTML = '<br><br><p><strong>Directions: Click a word and a sentence to make a match.</strong></p>';
 
 	searchTerms = $('#search').val().replace(/,\s+/g,",").split(",");
 
@@ -45,13 +61,12 @@ function main() {
 };
 
 
-function addClozeSentence(sen, correctWord, id) {
+function makeClozeSentence(sen, correctWord, id) {
 
 	var sentenceDiv = document.createElement('div');
-	sentenceDiv.innerHTML = sentenceNumber + '.' + "&nbsp&nbsp&nbsp" + sen + '<br><br>';
+	sentenceDiv.innerHTML = sen + '<br><br>';
 	sentenceDiv.className = 'row sentence';
 	sentenceDiv.setAttribute('name', correctWord + "-" + id);
-	document.getElementById('sentences').appendChild(sentenceDiv);
 
 	sentenceDiv.addEventListener("click", function() {
 
@@ -93,25 +108,16 @@ function addClozeSentence(sen, correctWord, id) {
 		}
 	})
 
-	sentenceNumber ++;
+	clozeSentences.push(sentenceDiv);
+
 	count ++;  
 }
 
 
 
-function resetGame() {
-	resetSelected();
-	document.getElementById('sentences').innerHTML = '';
-	document.getElementById('header').innerHTML = '';
-	wb.innerHTML = '';
-	wb.style.visibility = 'hidden';
-	examplesArray = [];
-}
-
-
 function win() {
 	alert("You win!");
-	resetGame();
+	resetSelected();
 }
 
 
@@ -152,7 +158,8 @@ function resetSelected() {
 function getSentences(wordsArray) {  //gets sentences with the target words from Tatoeba corpus JSON file
 
 
-	document.getElementById('sentences').innerHTML = "<h2>Loading...Please wait 10-20 seconds...</h2>"
+	document.getElementById('sentences').innerHTML = "<h2>Loading...Please wait up to 30 seconds...</h2><br><br>";
+
 
 	$.ajax({
 	    type : "GET",
@@ -164,9 +171,11 @@ function getSentences(wordsArray) {  //gets sentences with the target words from
 	    	document.getElementById('sentences').innerHTML = "";
 
 	    	var sentencesPerWord = document.getElementById('sentences-per-word').value;
-	    	var word;
+	    	
 	    	var endWord;
+	    	var word;
 
+	    	//for every word entered...
 	    	for (i=0; i<wordsArray.length; i++) {
 
 	    		counterForIds ++;
@@ -174,52 +183,8 @@ function getSentences(wordsArray) {  //gets sentences with the target words from
 	 	   		word = ' ' + wordsArray[i] + ' ';
 	    		endWord = ' ' + wordsArray[i] + '.';
 
-	    		//add to wordbank:
-	    		var wordDiv = document.createElement('div');
-	    		wordDiv.innerHTML = wordsArray[i];
-	    		wordDiv.className = 'word-div';
-	    		wordDiv.id = wordsArray[i] + "-" + counterForIds;
-	    		wb.appendChild(wordDiv);
-
-				wordDiv.addEventListener("click", function() {
-
-					if (!clickedWord) {
-						//if no sentence was selected, select this one
-						clickedWord = true;
-						this.style.backgroundColor = "lightgreen";
-						selectedWord = this.getAttribute('id');
-
-						//If this matches a word that's been clicked
-						if (clickedSentence) {
-							if(selectedSenName.split('-')[0] == selectedWord.split('-')[0]) {  //must use "includes" instead of equals, since the ids of the words and names of the sentences have numbers appended to them (sometimes more than one sentence per word)
-								correctAnswer();
-							}
-
-							//if it doesn't match
-							else {
-								this.style.backgroundColor = "white";
-								wrongAnswer();
-							}
-						}
-					}
-
-					// if this sentence was already selected, unselect it
-					else if (selectedWord == this.getAttribute('id')) {
-						clickedWord = false;
-						this.style.backgroundColor = "white";
-						selectedWord = null;
-					}
-
-					//if another sentence was selected, change the selection to this sentence
-					else {
-						document.getElementById(selectedWord).style.backgroundColor = "white";
-						this.style.backgroundColor = "lightgreen";
-						selectedWord = this.getAttribute('id');
-					}
-				}) ;
-
-
-	    		count = 0;
+	    		//First, check that the word actually exists in the corpus of sentences:
+	    		count = -1;
 
 	    		//search through the Tatoeba list of over 1,000,000 sentences:
 		    	for (j=0; j<response.sentences.length; j++) {
@@ -227,35 +192,114 @@ function getSentences(wordsArray) {  //gets sentences with the target words from
 					//if word is found in a sentence:
 		    		if (response.sentences[j].includes(word)) {
 
+		    			//add the word to wordbank:
+		    			makeWordDiv(wordsArray[i]);
+
 		    			var sentence = response.sentences[j].replace(word, " _______ ");
 
 		    			counterForNames++;
-
-						addClozeSentence(sentence, wordsArray[i], counterForNames);
+		    			//make a cloze sentence and add it to the clozeSentences array:
+						makeClozeSentence(sentence, wordsArray[i], counterForNames);
 						
-		    		};
+		    		}
 
 		    		//if word found at END of sentence:
-		    		if (response.sentences[j].includes(endWord)) {
+		    		else if (response.sentences[j].includes(endWord)) {
+
+		    			//add the word to wordbank:
+		    			makeWordDiv(wordsArray[i]);
 
 		    			var sentence = response.sentences[j].replace(endWord, " _______.");
 
 		    			counterForNames++;
+		    			//make a cloze sentence and add it to the clozeSentences array:
+		    			makeClozeSentence(sentence, wordsArray[i], counterForNames);
+		    		}
 
-		    			addClozeSentence(sentence, wordsArray[i], counterForNames);
-		    		};
-
-		    		//stop getting sentences after you've found enough!
+		    		//stop getting sentences after you've found enough:
 		    		if (count == sentencesPerWord) { break };
-		    	};
-		    };
+		    	};	
 
-		    noOfSentences = counterForNames;
+
+
+
+
+	   
+
+
+
+
+		    	noOfSentences = clozeSentences.length / 2;
+
+			}
+
+			addClozeDivs();
 		}
 	});
 }
 
 
+function makeWordDiv(wordToUse) {
+
+	//add to wordbank:
+	var wordDiv = document.createElement('div');
+	wordDiv.innerHTML = wordToUse;
+	wordDiv.className = 'word-div';
+	wordDiv.id = wordToUse + "-" + counterForIds;
+	wb.appendChild(wordDiv);
+
+	wordDiv.addEventListener("click", function() {
+
+		if (!clickedWord) {
+			//if no sentence was selected, select this one
+			clickedWord = true;
+			this.style.backgroundColor = "lightgreen";
+			selectedWord = this.getAttribute('id');
+
+			//If this matches a word that's been clicked
+			if (clickedSentence) {
+				if(selectedSenName.split('-')[0] == selectedWord.split('-')[0]) {  //must use "includes" instead of equals, since the ids of the words and names of the sentences have numbers appended to them (sometimes more than one sentence per word)
+					correctAnswer();
+				}
+
+				//if it doesn't match
+				else {
+					this.style.backgroundColor = "white";
+					wrongAnswer();
+				}
+			}
+		}
+
+		// if this sentence was already selected, unselect it
+		else if (selectedWord == this.getAttribute('id')) {
+			clickedWord = false;
+			this.style.backgroundColor = "white";
+			selectedWord = null;
+		}
+
+		//if another sentence was selected, change the selection to this sentence
+		else {
+			document.getElementById(selectedWord).style.backgroundColor = "white";
+			this.style.backgroundColor = "lightgreen";
+			selectedWord = this.getAttribute('id');
+		}
+	}) ;
+
+}
+
+
+function addClozeDivs() {
+	 // loop over clozeSentences to add the divs!
+	shuffle(clozeSentences);
+	
+	var n = 1;
+	for (i<0; i<clozeSentences.length; i++) {
+		//add the numbers:
+		clozeSentences[i].innerHTML = n + '.' + "&nbsp&nbsp&nbsp" + clozeSentences[i].innerHTML;
+		document.getElementById('sentences').appendChild(clozeSentences[i]);
+		n++;
+    };
+}
 
 
 //run the search when user presses Enter
